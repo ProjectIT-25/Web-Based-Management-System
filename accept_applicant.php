8397068b0ed7a32a7f75af4e18f4539fabcad49c
Kylie Cuadra <45311838+kyliecuadra@users.noreply.github.com>
2022-04-05 13:33:05 +0800

    Update accept_applicant.php

--- accept_applicant.php
+++ accept_applicant.php
@@ -36,31 +36,31 @@ if (isset($_GET['applicantID'])) {
     require("PHPMailer/src/SMTP.php");
     require("PHPMailer/src/Exception.php");
 
-    $mail = new PHPMailer\PHPMailer\PHPMailer();
-    $mail->IsSMTP(); 
+    $phpmailer = new PHPMailer\PHPMailer\PHPMailer();
+    $phpmailer->IsSMTP(); 
 
-    $mail->CharSet="UTF-8";
-    $mail->Host = "smtp.gmail.com";
-    $mail->Port = 465; //465 or 587
+    $phpmailer->CharSet="UTF-8";
+    $phpmailer->Host = "smtp.gmail.com";
+    $phpmailer->Port = 465; //465 or 587
 
-    $mail->SMTPSecure = 'ssl';  
-    $mail->SMTPAuth = true; 
-    $mail->IsHTML(true);
+    $phpmailer->SMTPSecure = 'ssl';  
+    $phpmailer->SMTPAuth = true; 
+    $phpmailer->IsHTML(true);
 
     //Authentication
-    $mail->Username = "departmentmcc@gmail.com"; //your email
-    $mail->Password = 'mccdepartment'; //your password
+    $phpmailer->Username = "departmentmcc@gmail.com"; //your email
+    $phpmailer->Password = 'mccdepartment'; //your password
 
     //Set Params
-    $mail->SetFrom("departmentmcc@gmail.com"); //sender email
-    $mail->AddAddress($email); //receiver email
-    $mail->IsHTML(true);
-    $mail->Subject = "Account Registration";
-    $mail->Body = "<h1>Your account has been registered!</h1><br><h3>Here is your login credentials</h3><br>Email: $email<br>Password: $password";
+    $phpmailer->SetFrom("departmentmcc@gmail.com"); //sender email
+    $phpmailer->AddAddress($email); //receiver email
+    $phpmailer->IsHTML(true);
+    $phpmailer->Subject = "Account Registration";
+    $phpmailer->Body = "<h1>Your account has been registered!</h1><br><h3>Here is your login credentials</h3><br>Email: $email<br>Password: $password";
 
 
-     if(!$mail->Send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
+     if(!$phpmailer->Send()) {
+        echo "Mailer Error: " . $phpmailer->ErrorInfo;
     } else {
         echo "<script>window.location.href='admin/pending-accounts.php';</script>";
     }
