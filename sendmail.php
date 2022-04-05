7bbd6c0aa4f74a3c88660d5160d62d72c24d2e08
Jansen <jansen.oribello@cvsu.edu.ph>
2022-04-06 05:19:05 +0800

    minimal changes in email

--- sendmail.php
+++ sendmail.php
@@ -4,6 +4,8 @@ include ("connection.php");
   $email=$_GET['email'];
   $password=$_GET['password'];
   $fname=$_GET['fname'];
+
+
   require 'sendmail/vendor/autoload.php';
   use \Mailjet\Resources;
   $mj = new \Mailjet\Client('5a2d7d67ea2951cde296436e8e58f739','64faad233ff131053131226ae530b5a7',true,['version' => 'v3.1']);
@@ -23,9 +25,9 @@ include ("connection.php");
 
           ]
         ],
-        'Subject' => "MCC Account Registration",
+        'Subject' => "Account Registration",
         'TextPart' => "Dear $fname,",
-        'HTMLPart' => "<h2>You have been accepted to <a href='https://document-management-systems.herokuapp.com/'>MCC</a>!</h2><h3>Here is your login credentials.</h3><h4>Email: $email<br>Password: <i>$password</i></h4>"
+        'HTMLPart' => "<h2>Dear $fname,<br>You have been accepted to <a href='https://document-management-systems.herokuapp.com/'>MCC</a>!</h2><h3>Here is your login credentials:</h3><h4>Email: $email<br>Password: <i>$password</i></h4>"
       ]
     ]
   ];
